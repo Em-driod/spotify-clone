6c15edbc4ca975d1469165d5e518e9e26bf1050a
Em-driod <sheenleen2@gmail.com>
2025-07-02 16:27:40 +0100

    mandem

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-
-import LandingPage from './pages/LandingPage';
 import Welcome from './pages/welcome';
+import LandingPage from './pages/LandingPage';
+
 import Skips from './pages/Skips';
 
 const AnimatedRoutes = () => {
@@ -13,7 +13,7 @@ const AnimatedRoutes = () => {
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<LandingPage />} />
         <Route path="/skip" element={<Skips />} />
-        <Route path="/welcome" element={<Welcome />} />
+       <Route path="/welcome" element={<Welcome />} />
       
       </Routes>
     </AnimatePresence>
